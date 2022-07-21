2003ade56a001dd4addc54e6bec18c1f048ab538
janinelps <janinelpss@gmail.com>
2022-07-22 00:23:20 +0100

    implementa async await

--- src/controllers/login/index.ts
+++ src/controllers/login/index.ts
@@ -4,8 +4,8 @@ import { Login } from "@models/entity/Login"
 export const getLogin = async (request, response) => {
   try {
     const loginRepository = getRepository(Login)
-    let user = await loginRepository.find()
-    return response.status(200).json(user)
+    let login = await loginRepository.find()
+    return response.status(200).json(login)
   } catch (error) {
     return response.status(500).json(error)
   }
--- src/controllers/movie/index.ts
+++ src/controllers/movie/index.ts
@@ -1,10 +1,10 @@
 import { getRepository } from "typeorm"
 import { Movie } from "@models/entity/Movie"
 
-export const getMovie = (request, response) => {
+export const getMovie = async (request, response) => {
   try {
     const movieRepository = getRepository(Movie)
-    const movie = movieRepository.find()
+    const movie = await movieRepository.find()
     return response.status(200).json(movie)
   } catch (error) {
     return response.status(500).json(error)
