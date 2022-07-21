de07bdac0969843a1b02f7a031167425a592b969
janinelps <janinelpss@gmail.com>
2022-07-22 00:20:40 +0100

    implementacao async await

--- src/controllers/movie/index.ts
+++ src/controllers/movie/index.ts
@@ -1,19 +1,19 @@
 import { getRepository } from "typeorm"
 import { Movie } from "@models/entity/Movie"
 
-export const getMovie = (request, response) => {
+export const getMovie = async (request, response) => {
   try {
     const movieRepository = getRepository(Movie)
-    const movie = movieRepository.find()
+    const movie = await movieRepository.find()
     return response.status(200).json(movie)
   } catch (error) {
     return response.status(500).json(error)
   }
 }
 
-export const getMovieId = (request, response) => {
-  const id = request.params.id
+export const getMovieId = async (request, response) => {
+  const { id } = request.params
   const repositoryMovie = getRepository(Movie)
-  const movie = repositoryMovie.findOne(id)
+  const movie = await repositoryMovie.findOne(id)
   return response.json({ movie })
 }
