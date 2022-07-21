36c968f346bc644704332aaecf947a7549e4b499
janinelps <janinelpss@gmail.com>
2022-07-21 22:27:37 +0100

    implementacao movieRepository

--- src/controllers/todo/index.ts
+++ src/controllers/todo/index.ts
@@ -1,3 +1,6 @@
+import { getRepository } from "typeorm"
+import { ToDo } from "@models/entity/ToDo"
+
 export const itsWorks = (request, response) => {
   return response.json({ message: "It's Works!!" })
 }
@@ -6,6 +9,8 @@ export const getTODO = (request, response) => {
   return response.json({ todos: [] })
 }
 
-export const getMovie = (require, response) => {
-  return response.json({ movie: [] })
+export const getMovie = (request, response) => {
+  const movieRepository = getRepository(ToDo)
+  const movie = movieRepository.find()
+  return response.json(movie)
 }
