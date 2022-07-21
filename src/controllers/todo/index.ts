2d2caddec4a25a6b02b7951086f2ddf7a63f49bf
janinelps <janinelpss@gmail.com>
2022-07-21 18:53:57 +0100

    inicio implementacao endpoint movieId

--- src/controllers/todo/index.ts
+++ src/controllers/todo/index.ts
@@ -1,3 +1,6 @@
+import { ToDo } from "@models/entity/ToDo"
+import { getRepository } from "typeorm"
+
 export const itsWorks = (request, response) => {
   return response.json({ message: "It's Works!!" })
 }
@@ -6,9 +9,17 @@ export const getTODO = (request, response) => {
   return response.json({ todos: [] })
 }
 
-export const getMovie = (require, response) => {
+export const getMovie = (request, response) => {
   return response.json({ movie: [] })
 }
-export const getLogin = (require, response) => {
+
+export const getLogin = (request, response) => {
   return response.json({ movie: [] })
+}
+
+export const getMovieId = (request, response) => {
+  const id = request.params.id
+  const repositoryMovie = getRepository(ToDo)
+  const movie = repositoryMovie.findOne(id)
+  return response.json({ movie })
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,15 @@
-import { getLogin, getMovie, getTODO, itsWorks } from "@controllers/todo"
+import {
+  getLogin,
+  getMovie,
+  getMovieId,
+  getTODO,
+  itsWorks,
+} from "@controllers/todo"
 
 export const defineRoutes = (app) => {
   app.get("/", itsWorks)
   app.get("/todo", getTODO)
   app.get("/movie", getMovie)
   app.get("/login", getLogin)
+  app.get("/movie/:id", getMovieId)
 }
